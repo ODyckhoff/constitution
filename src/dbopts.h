ec5c76adfdccf780b824e0a02273bb8d38d3c447
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2015-09-03 16:45:55 +0100

    Re-added dbopts.h file

---
+++ src/dbopts.h
@@ -0,0 +1,14 @@
+#ifndef __CNSTN_DB_OPTS_H
+#define __CNSTN_DB_OPTS_H
+
+/* Local Database Options. */
+
+    #define DBUSER "root"
+    #define DBHOST "localhost"
+    #define DBPASS "password"
+    #define DBNAME "my_db"
+
+    #define DRIVER DB_MYSQL
+
+#endif /* __CNSTN_DB_OPTS_H */
+
